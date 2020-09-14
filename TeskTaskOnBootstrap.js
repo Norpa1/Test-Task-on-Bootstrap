b6249c48cc84150a884b9d81252648ed87f324b3
Norpa <norpa344@gmail.com>
2020-09-14 17:25:42 +0400

    Some issues has been changed

--- TeskTaskOnBootstrap.js
+++ TeskTaskOnBootstrap.js
@@ -1,77 +1,71 @@
-setTimeout( () => {
-const MyNav = document.getElementsByClassName("MyNavigation")[0]
-const menuBtn = document.getElementsByClassName("btn-wrapper")[0]
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    document.getElementsByClassName("btn-wrapper open")[0].style.zIndex = "100";
-    MyNav.setAttribute("class","nav navbar-nav MyNavigation")
-    document.getElementsByClassName("my-nav-wrapper")[0].className = "d-flex d-md-none my-nav-wrapper-active bg-white container col-10 position-absolute"; 
-    document.getElementsByClassName("CityInfo")[0].style.display = "none";
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    MyNav.setAttribute("class","nav navbar-nav ml-5 MyNavigation")
-    document.getElementsByClassName("my-nav-wrapper-active")[0].className = "my-nav-wrapper d-flex d-md-none bg-white container col-10 position-absolute"; 
-    setTimeout(() => {
-      if (document.getElementsByClassName("my-nav-wrapper").length > 0) {
-        document.getElementsByClassName("my-nav-wrapper")[0].className = "my-nav-wrapper bg-white container col-10 position-absolute d-none"; 
-      }
-    },900)
-    document.getElementsByClassName("CityInfo")[0].style.display = "flex";
-    menuOpen = false;
-  }
-})
-
-  let DefaultNewsQuantity = 3;
-  let NewsArray = [];
-  let DateArray = [];
-  for (var i=0;i<document.getElementsByClassName("dboutput1").length;i++) {
-    NewsArray.push(document.getElementsByClassName("dboutput1")[i].innerHTML);  
-    DateArray.push(document.getElementsByClassName("dboutput2")[i].innerHTML);
-  }
-  for (var i=0;i<document.getElementsByClassName("dboutput1").length;i++) {
-    document.getElementsByClassName("dboutput1")[i].remove();
-    document.getElementsByClassName("dboutput2")[i].remove(); 
-  }
-
-  for (let i=0;i<DefaultNewsQuantity;i++) {
-    window["ItemHTMLCode"+i] = "";
-    var WrapperElem =  document.createElement("div");
-    WrapperElem.className = "d-block w-100 d-flex justify-content-around";
-
-    for (let k=0;k<DefaultNewsQuantity;k++) {
-      var elem = document.createElement("div");
-      elem.className = "col-3 bg-white";
-      elem.style.padding = "35px";
-      elem.style.border = "2px solid #D8D8D8";
-      let elem2 = document.createElement("span");
-      elem2.innerHTML = DateArray[k];
-      let elem3 = document.createElement("div");
-      elem3.className = "font-weight-bold mb-3 mt-3";
-      elem3.innerHTML = NewsArray[k];
-      let elem4 = document.createElement("button");
-      elem4.className = "btn btn-outline-danger col-xs-btn-xs btn-lg";
-      elem4.innerHTML = "Подробнее";
-
-      elem.appendChild(elem2)
-      elem.appendChild(elem3)
-      elem.appendChild(elem4)
-     WrapperElem.appendChild(elem)
+document.addEventListener('DOMContentLoaded', function() { 
+  const MyNav = document.getElementsByClassName("MyNavigation")[0]
+  const menuBtn = document.getElementsByClassName("btn-wrapper")[0]
+  let menuOpen = false;
+  menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+      menuBtn.classList.add('open');
+      MyNav.setAttribute("class","nav navbar-nav MyNavigation")
+      document.getElementsByClassName("my-nav-wrapper")[0].className = "d-flex d-md-none my-nav-wrapper-active bg-white container col-10 position-absolute"; 
+      document.getElementsByClassName("CityInfo")[0].className = "CityInfo d-none"
+      menuOpen = true;
+    } else {
+      menuBtn.classList.remove('open');
+      MyNav.setAttribute("class","nav navbar-nav ml-5 MyNavigation")
+      document.getElementsByClassName("my-nav-wrapper-active")[0].className = "my-nav-wrapper d-flex d-md-none bg-white container col-10 position-absolute"; 
+      setTimeout(() => {
+        if (document.getElementsByClassName("my-nav-wrapper").length > 0) {
+          document.getElementsByClassName("my-nav-wrapper")[0].className = "my-nav-wrapper bg-white container col-10 position-absolute d-none"; 
+        }
+      },1000)
+      document.getElementsByClassName("CityInfo")[0].className = "nav navbar-nav CityInfo d-flex"
+      menuOpen = false;
     }
-     
-    NewsArray.push(NewsArray[0]); 
-    DateArray.push(DateArray[0]);    
-    NewsArray.splice(0,1); 
-    DateArray.splice(0,1); 
-    
-    let Element = document.getElementsByClassName("MyInnerCarousel")[0].children[i];
-    Element.appendChild(WrapperElem);
+  })
 
-  }
+    let DefaultNewsQuantity = 3;
+    let NewsArray = [];
+    let DateArray = [];
+    for (var i=0;i<document.getElementsByClassName("dboutput1").length;i++) {
+      NewsArray.push(document.getElementsByClassName("dboutput1")[i].innerHTML);  
+      DateArray.push(document.getElementsByClassName("dboutput2")[i].innerHTML);
+    }
+    for (var i=0;i<document.getElementsByClassName("dboutput1").length;i++) {
+      document.getElementsByClassName("dboutput1")[i].remove();
+      document.getElementsByClassName("dboutput2")[i].remove(); 
+    }
 
+    for (let i=0;i<DefaultNewsQuantity;i++) {
+      window["ItemHTMLCode"+i] = "";
+      var WrapperElem =  document.createElement("div");
+      WrapperElem.className = "d-block w-100 d-flex justify-content-around";
 
-},500)
+      for (let k=0;k<DefaultNewsQuantity;k++) {
+        var elem = document.createElement("div");
+        elem.className = "col-3 bg-white My-news-wrapper";
+        let elem2 = document.createElement("span");
+        elem2.innerHTML = DateArray[k];
+        let elem3 = document.createElement("div");
+        elem3.className = "font-weight-bold mb-5 mt-3";
+        elem3.innerHTML = NewsArray[k];
+        let elem4 = document.createElement("button");
+        elem4.className = "btn btn-outline-danger col-xs-btn-xs btn-lg My-btn-more-custom";
+        elem4.innerHTML = "Подробнее";
 
+        elem.appendChild(elem2)
+        elem.appendChild(elem3)
+        elem.appendChild(elem4)
+      WrapperElem.appendChild(elem)
+      }
+      
+      NewsArray.push(NewsArray[0]); 
+      DateArray.push(DateArray[0]);    
+      NewsArray.splice(0,1); 
+      DateArray.splice(0,1); 
+      
+      let Element = document.getElementsByClassName("MyInnerCarousel")[0].children[i];
+      Element.appendChild(WrapperElem);
+
+    }
+});
  
--- TestTaskOnBootstrap.php
+++ TestTaskOnBootstrap.php
@@ -7,163 +7,156 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Test task on Bootstrap</title>
-	<script src="TeskTaskOnBootstrap.js"></script>
 	<link rel="stylesheet" href="TestTaskOnBootstrap.css">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
 </head>
 <body >
 
-	<header id="slide-out1" style="flex-direction:column;" class="d-none bg-white container col-10 position-absolute my-nav-wrapper"> 
-	
-		<div style="margin-top:100px; margin-bottom:70px;">
-			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h5 style="font-size:1.35em; line-height:2">Услуги</h5></a>
-			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h5 style="font-size:1.35em; line-height:2">Портфолио</h5></a>
-			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h5 style="font-size:1.35em; line-height:2">Отзывы</h5></a>
-			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h5 style="font-size:1.35em; line-height:2">Вакансии</h5></a>
-			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h5 style="font-size:1.35em; line-height:2">Контакты</h5></a>	
+	<header class="d-none bg-white container col-10 position-absolute my-nav-wrapper"> 
+		<div class="My-nav-list">
+			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h4 class="mb-3">Услуги</h4></a>
+			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h4 class="mb-3">Портфолио</h4></a>
+			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h4 class="mb-3">Отзывы</h4></a>
+			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h4 class="mb-3">Вакансии</h4></a>
+			<a class="ml-3 text-dark nav-link font-weight-bold" href="#"><h4 class="mb-3">Контакты</h4></a>	
 		</div>
-		<div style="height:100vh; background-color: #211F2A">
+		<div class="My-nav-footer">
 			<a class="ml-3 mt-4 text-white nav-link d-flex flex-column justify-content-end" href="#">
-				<h6   style="font-size:1.1em; ">Ростов-на-Дону,</h6>
-				<h6   style="font-size:1.1em; ">Ленина, 21</h6>
-				<span style="font-size:1.4em; line-height:2.1" class="font-weight-bold">8(863)243-15-10</span>
+				<h5>Ростов-на-Дону,</h5>
+				<h5>Ленина, 21</h5>
+				<span class="font-weight-bold mt-2"><h3>8(863)243-15-10</h3></span>
 			</a>
 		</div>
-	 
 	</header>
-	<div style="min-height:100vh;">	
-	<button type="button" data-toggle="collapse" data-target="#slide-out" class="d-flex d-md-none navbar-toggler custom-toggler">
-		<div class="btn-wrapper">
-			<div class="menu-btn-burger"></div>
-		</div>
-	</button>
 
-	<nav style="position:relative; z-index:15;" class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
-		
-		<div class="container col-10"> 
- 
-			<div class="col-0.1"></div>
-
-			<a href="#" class="navbar-brand col-md-order-1">
-				<img width="48" height="48" src="rededge_logo.png" alt="">	
-			</a>
-
-			<div class="collapse navbar-collapse col-md-order-2" >
-				<ul class="nav navbar-nav ml-5 MyNavigation">
-
-					<li class="nav-item mx-2">
-						<a class="nav-link font-weight-bold" href="#"><h5 class="text-dark" style="font-size:1.35em">Услуги</h5></a>
-					</li>
-			 		
-			 		<li class="nav-item mx-2">
-						<a class="nav-link font-weight-bold" href="#"><h5 class="text-dark" style="font-size:1.35em">Портфолио</h5></a>
-					</li>
-
-			 		<li class="nav-item mx-2">
-						<a class="nav-link font-weight-bold" href="#"><h5 class="text-dark" style="font-size:1.35em">Отзывы</h5></a>
-					</li>
-
-				 	<li class="nav-item mx-2">
-						<a class="nav-link font-weight-bold" href="#"><h5 class="text-dark" style="font-size:1.35em">Вакансии</h5></a>
-					</li>	
-
-					<li class="nav-item mx-2">
-						<a class="nav-link font-weight-bold" href="#"><h5 class="text-dark" style="font-size:1.35em">Контакты</h5></a>
-					</li>	
-
-				</ul>
+	<div class="My-main-wrapper">	
+		<button type="button" data-toggle="collapse" class="d-flex d-md-none navbar-toggler custom-toggler"> 
+			<div class="btn-wrapper">
+				<div class="menu-btn-burger"></div>
 			</div>
-			
-			<div class="navbar-brand col-md-order-3">
-				<ul class="nav navbar-nav CityInfo">
-					<li class="">
-						<a class="text-dark nav-link d-flex flex-column justify-content-end" href="#">
-							<span class="text-dark font-weight-bold">8(863)243-15-10</span>
-							<h6>Ростов-на-Дону</h6>
-						</a>	
-					</li>	
-				</ul>
-			</div>
-		</div>
- 
-	</nav>
+		</button>
 
-	<div style="background: linear-gradient(0deg, rgba(53, 53, 53, 0.5), rgba(53, 53, 53, 0.5)), url('Rectangle2.png') no-repeat; background-size:cover; width:2000px; height: 600px; z-index:3; left:0px;" class="d-none d-md-block container-fluid position-relative"> 	 
-		<div class="container col-10 w-100 h-100">
-				<div class="col-5 w-100 h-100">
-					<div class="h-100 w-100 row d-flex align-items-center">
-						<div class="container">
-							<div class="text-white"><h1>Рекламно-информационное агентство</h1></div>
-							<div class="text-white mb-4"><h5>Будем рады, если Вы обратитесь в наше Агентство. Мы готовы предложить Вам передовые решения для продвижения Вашего бизнеса.</h5></div>
-							<form class="" action="">
-								<input class="mb-3 mr-3 form-control-lg" placeholder="Номер телефона" type="text">
-								<button style="background-color: #D51522; width: 256px;" class="mb-2 btn btn-danger btn-lg">Обратный звонок</button>
-							</form>
-						</div>  
-				 
-					</div>	
+		<nav class="navbar navbar-expand-md navbar-light bg-light My-header-custom" role="navigation">
+			<div class="container-fluid d-flex justify-content-center">	 
+			<div class="col-10 d-flex"> 
+				<div class=""></div>
+				<a href="#" class="mx-auto navbar-brand col-md-order-1 d-flex align-items-center">
+					<img class="Main-logo" src="rededge_logo.png" alt="">	
+				</a>
+				<div class="collapse navbar-collapse col-md-order-2" >
+					<ul class="nav navbar-nav ml-5 MyNavigation">
+						<li class="nav-item mx-3">
+							<a class="nav-link font-weight-bold" href="#"><h4 class="text-dark">Услуги</h4></a>
+						</li>		 		
+						<li class="nav-item mx-3">
+							<a class="nav-link font-weight-bold" href="#"><h4 class="text-dark">Портфолио</h4></a>
+						</li>
+						<li class="nav-item mx-3">
+							<a class="nav-link font-weight-bold" href="#"><h4 class="text-dark">Отзывы</h4></a>
+						</li>
+						<li class="nav-item mx-3">
+							<a class="nav-link font-weight-bold" href="#"><h4 class="text-dark">Вакансии</h4></a>
+						</li>	
+						<li class="nav-item mx-3">
+							<a class="nav-link font-weight-bold" href="#"><h4 class="text-dark">Контакты</h4></a>
+						</li>	
+					</ul>
+				</div>		
+				<div class="ml-auto navbar-brand col-md-order-3">
+					<ul class="nav navbar-nav CityInfo">
+						<li>
+							<a class="text-dark nav-link d-flex flex-column justify-content-end" href="#">
+								<span class="text-dark font-weight-bold">8(863)243-15-10</span>
+								<h6 class="ml-auto">Ростов-на-Дону</h6>
+							</a>	
+						</li>	
+					</ul>
 				</div>
-			</div>	
-			
-			 
-		</div>
+			</div>
 
-		<?php 
-			if ($resultCheck > 0) {
-				while ($row = mysqli_fetch_assoc($result)) {
-					echo "<div class='dboutput1' style='display:none'>".$row["NewsInfo"]."</div>";
-					echo "<div class='dboutput2' style='display:none'>".$row["NewsData"]."</div>";
+
+			</div>
+		</nav>
+
+		<div class="d-none d-md-flex justify-content-center container-fluid position-relative My-connect-form"> 	 
+			<div class="h-100 col-10">
+					<div class="d-flex justify-content-end col-6 h-100">
+						<div class="h-100 w-100 row d-flex align-items-center">
+							<div class="container-fluid">
+								<div class="text-white"><h1 class="My-main-info-custom">Рекламно-информационное агентство</h1></div>
+								<div class="col-10 d-flex justify-content-start text-white mb-4 My-info-custom">
+									<h5 class="My-info-custom font-weight-normal">Будем рады, если Вы обратитесь в наше Агентство. Мы готовы предложить Вам передовые решения для продвижения Вашего бизнеса.</h5>
+								</div>
+								<form class="mt-5" action="">
+									<input class="mb-3 mr-3 form-control-lg My-input-custom" placeholder="Номер телефона" type="text">
+									<button class="mb-2 btn btn-danger btn-lg My-button-custom">Обратный звонок</button>
+								</form>
+							</div>  
+					
+						</div>	
+					</div>
+				</div>		 
+			</div>
+
+			<?php 
+				if ($resultCheck > 0) {
+					while ($row = mysqli_fetch_assoc($result)) {
+						echo "<div class='dboutput1'>".$row["NewsInfo"]."</div>";
+						echo "<div class='dboutput2'>".$row["NewsData"]."</div>";
+					}
 				}
-			}
-		?>
+			?>
 		
-			<div class="d-none d-md-block" style="margin-top:90px"> 
+			<div class="d-none d-md-block mt-5"> 
 				<div class="container-fluid position-relative mb-5 d-flex justify-content-center">
 					<h1 class="text-black font-weight-bold">Новости</h1>
 				</div>		 
 			</div>
 
-				<div id="carouselExampleIndicators" class="carousel slide d-none d-md-block" data-ride="carousel">
-					<div style="position:absolute; top:15;" class="d-block w-100 h-100 d-flex justify-content-around">	
-						<div class="col-3 h-100" style=" box-shadow: -25px -25px 0px 0px rgba(213,21,34,1)"></div>
-						<div class="col-3 h-100"></div>					
-						<div class="col-3 h-100" style="box-shadow: 25px 25px 0px 0px rgba(111,97,160,1)"></div>
-					</div>
-					<div class="MyInnerCarousel carousel-inner">
-						<div class="carousel-item active"></div>
-						<div class="carousel-item"></div>
-					  	<div class="carousel-item"></div>
-					</div>
-					<ol class="carousel-indicators MyIndicators">
-					  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-					  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-					  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-					</ol>
+			<div id="carouselExampleIndicators" class="carousel slide d-none d-md-block col-10 mx-auto" data-ride="carousel">
+				<div class="d-block w-100 h-100 d-flex justify-content-around position-absolute">	
+					<div class="col-3 h-100 My-frame-custom-start" ></div>
+					<div class="col-3 h-100"></div>					
+					<div class="col-3 h-100 My-frame-custom-end"></div>
 				</div>
+				<div class="MyInnerCarousel carousel-inner">
+					<div class="carousel-item active"></div>
+					<div class="carousel-item"></div>
+					<div class="carousel-item"></div>
+				</div>
+				<ol class="carousel-indicators MyIndicators">
+					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="mr-3 active"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="1" class="mr-3"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="2" class="mr-3"></li>
+				</ol>
+			</div>
+
 			</div>
 	</div>
-		<div class="MyFooter footer mt-auto w-100 h-100 container-fluid d-none d-md-block">
-			<div class="container col-10">
-				<div class="row">
-					<a href="#" class="col mt-5 mb-5">
-						<img width="64" height="64" src="rededge_logo.png" alt="">	
-					</a>
-					<div class="col-8 d-flex align-items-center">
-						<div class="row">
-							<a class="nav-link font-weight-bold" href="#"><h5 class="text text-white">Услуги</h5></a>
-							<a class="nav-link font-weight-bold" href="#"><h5 class="text text-white">Портфолио</h5></a>
-							<a class="nav-link font-weight-bold" href="#"><h5 class="text text-white">Отзывы</h5></a>
-							<a class="nav-link font-weight-bold" href="#"><h5 class="text text-white">Вакансии</h5></a>
-							<a class="nav-link font-weight-bold" href="#"><h5 class="text text-white">Контакты</h5></a>
-						</div>
-					</div>	 
-				</div>
+
+ 
+
+	<div class="MyFooter footer w-100 h-100 container-fluid d-none d-md-flex justify-content-center">
+		<div class="d-flex col-10">
+			<div class="row">
+				<a href="#" class="col-1 mt-5 mb-5">
+					<img class="My-logo-footer" src="rededge_logo.png" alt="">	
+				</a>
+				<div class="col-10 d-flex justify-content-center align-items-center">
+					<div class="d-flex flex-row">
+						<a class="nav-link" href="#"><h5 class="text text-white font-weight-normal">Услуги</h5></a>
+						<a class="nav-link" href="#"><h5 class="text text-white font-weight-normal">Портфолио</h5></a>
+						<a class="nav-link" href="#"><h5 class="text text-white font-weight-normal">Отзывы</h5></a>
+						<a class="nav-link" href="#"><h5 class="text text-white font-weight-normal">Вакансии</h5></a>
+						<a class="nav-link" href="#"><h5 class="text text-white font-weight-normal">Контакты</h5></a>
+					</div>
+				</div>	 
 			</div>
 		</div>
- 
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	</div>
+	<script src="TeskTaskOnBootstrap.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 </body>
 </html>
 
